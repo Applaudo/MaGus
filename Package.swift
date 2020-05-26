b6e1d433c27a1d36a685bd1b58b18153bfd6ff59
Gustavo Campos <guseducampos@gmail.com>
2020-05-26 13:29:22 -0600

    :arrow_up: use TOMLDecoder dependency from main repo

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
         .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMinor(from: "0.13.0")),
         .package(url: "https://github.com/kylef/PathKit", from: "0.9.0"),
-        .package(url: "https://github.com/guseducampos/TOMLDecoder.git", .branch("prevent-crash")),
+        .package(url: "https://github.com/dduan/TOMLDecoder.git", from: "0.1.6"),
         .package( url: "https://github.com/Flight-School/AnyCodable", from: "0.2.3"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "3.0.1")
     ],
